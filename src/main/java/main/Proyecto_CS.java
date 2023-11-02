9bb5c50fb85421d60ee407c825aacf4579db3756
luisfer128 <81640917+luisfer128@users.noreply.github.com>
2023-11-01 19:08:36 -0500

    Add files via upload

---
+++ src/main/java/main/Proyecto_CS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+/**
+ *
+ * @author Luis
+ */
+public class Proyecto_CS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
